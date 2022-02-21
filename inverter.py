bdd2c8abf630b1a0f1839e5d1868b2a069389c73
Matej Pristak <matej.pristak@gmail.com>
2022-02-21 19:09:03 +0100

    more fields

--- inverter.py
+++ inverter.py
@@ -1,4 +1,6 @@
 import asyncio
+import time
+
 import goodwe
 from config import influxConfig, inverterConfig
 from influxdb_client import InfluxDBClient, Point
@@ -14,18 +16,24 @@ async def store_runtime_data():
 
     write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("string", "1").field("power", runtime_data["ppv1"]))
     write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("string", "2").field("power", runtime_data["ppv2"]))
-    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "total").field("power", runtime_data["ppv"]))
-    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "total").field("direction", runtime_data["grid_in_out_label"]))
-    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "total").field("load", runtime_data["load_ptotal"]))
-    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "today").field("in", runtime_data["e_day_imp"]))
-    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "today").field("out", runtime_data["e_day_exp"]))
-    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "total").field("in", runtime_data["e_total_imp"]))
-    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "total").field("out", runtime_data["e_total_exp"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("string", "all").field("power", runtime_data["ppv"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").field("direction", runtime_data["grid_in_out_label"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "now").field("consumption", runtime_data["house_consumption"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").field("battery_load", runtime_data["pbattery1"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "total").field("battery_charged", runtime_data["e_bat_charge_total"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "today").field("battery_charged", runtime_data["e_bat_charge_day"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "total").field("battery_discharged", runtime_data["e_bat_discharge_total"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "today").field("battery_discharged", runtime_data["e_bat_discharge_day"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "today").field("generation", runtime_data["e_day"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").tag("sum", "today").field("consumption_day", runtime_data["e_load_day"]))
     write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").field("soc", runtime_data["battery_soc"]))
+    write_api.write(bucket=influxConfig["bucket"], record= Point("FVE").field("soh", runtime_data["battery_soh"]))
 
 #    for sensor in inverter.sensors():
 #        if sensor.id_ in runtime_data:
 #            print(f"{sensor.id_}: \t\t {sensor.name} = {runtime_data[sensor.id_]} {sensor.unit}")
 
 
-asyncio.run(store_runtime_data())
+while True:
+    asyncio.run(store_runtime_data())
+    time.sleep(15)
--- wu.py
+++ wu.py
@@ -1,3 +1,5 @@
+import asyncio
+import time
 from pprint import pprint
 from config import wundergroundConfig, influxConfig
 
@@ -11,19 +13,26 @@ wu = WUndergroundAPI(
     units=units.METRIC_UNITS,
 )
 
-w = wu.current()["observations"][0]
 
-# pprint(w)
+async def store_runtime_data():
+    w = wu.current()["observations"][0]
 
-client = InfluxDBClient(url=influxConfig["url"], token=influxConfig["token"], org=influxConfig["org"])
-write_api = client.write_api(write_options=SYNCHRONOUS)
+    # pprint(w)
 
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("humidity", w["humidity"]))
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("temperature", w["metric"]["temp"]))
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("windChill", w["metric"]["windChill"]))
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("windSpeed", w["metric"]["windSpeed"]))
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("windGust", w["metric"]["windGust"]))
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("precipRate", w["metric"]["precipRate"]))
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("precipTotal", w["metric"]["precipTotal"]))
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("solarRadiation", w["solarRadiation"]))
-write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("winddir", w["winddir"]))
+    client = InfluxDBClient(url=influxConfig["url"], token=influxConfig["token"], org=influxConfig["org"])
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("humidity", w["humidity"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("temperature", w["metric"]["temp"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("windChill", w["metric"]["windChill"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("windSpeed", w["metric"]["windSpeed"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("windGust", w["metric"]["windGust"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("precipRate", w["metric"]["precipRate"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("precipTotal", w["metric"]["precipTotal"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("solarRadiation", w["solarRadiation"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("winddir", w["winddir"]))
+
+
+while True:
+    asyncio.run(store_runtime_data())
+    time.sleep(60)
