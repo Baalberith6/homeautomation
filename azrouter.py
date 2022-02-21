5d1ae7454f699d0d4273efa6aa4c9faa7596e9c6
Matej Pristak <matej.pristak@gmail.com>
2022-02-21 21:21:08 +0100

    azrouter

---
+++ azrouter.py
@@ -0,0 +1,32 @@
+import asyncio
+import time
+from pprint import pprint
+
+import requests
+
+from config import azrouterConfig, influxConfig
+
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+
+def _request(path: tuple):
+    """Forward a raw request to Weather Underground APIs"""
+    r = requests.get(azrouterConfig["ip_address"] + '/'.join(path))
+    return r.json()
+
+
+async def store_runtime_data():
+    r = _request(("api", "v1", "power"))
+
+    # pprint(r)
+
+    client = InfluxDBClient(url=influxConfig["url"], token=influxConfig["token"], org=influxConfig["org"])
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
+    write_api.write(bucket=influxConfig["bucket"], record=Point("FVE").tag("sum", "today").field("saved_energy", r["output"]["energy"][4]["value"]))
+    write_api.write(bucket=influxConfig["bucket"], record=Point("FVE").field("saved_energy", r["output"]["power"][0]["value"]))
+
+while True:
+    asyncio.run(store_runtime_data())
+    time.sleep(15)
--- config.py
+++ config.py
@@ -9,6 +9,10 @@ inverterConfig = {
     "ip_address": "192.168.1.77"
 }
 
+azrouterConfig = {
+    "ip_address": "http://192.168.1.167/"
+}
+
 wundergroundConfig = {
     "apiKey": "TODO",
     "stationId": "ISTRMI1"
