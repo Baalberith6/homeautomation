638a8f8d6ef29cffcb797df778347caeb63f7133
Matej Pristak <matej.pristak@gmail.com>
2022-02-22 15:35:37 +0100

    fix co2

--- co2.py
+++ co2.py
@@ -13,17 +13,16 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 async def store_runtime_data():
     sgp30 = seeed_sgp30.grove_sgp30(Bus())
+    client = InfluxDBClient(url=influxConfig["url"], token=influxConfig["token"], org=influxConfig["org"])
+    write_api = client.write_api(write_options=SYNCHRONOUS)
 
     while True:
         data = sgp30.read_measurements()
         co2_eq_ppm, tvoc_ppb = data.data
-        print(co2_eq_ppm)
-        time.sleep(10)
 
-    #client = InfluxDBClient(url=influxConfig["url"], token=influxConfig["token"], org=influxConfig["org"])
-    #write_api = client.write_api(write_options=SYNCHRONOUS)
+        write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("co2", co2_eq_ppm))
 
-    #write_api.write(bucket=influxConfig["bucket"], record=Point("Weather").field("co2", co2_eq_ppm))
+        time.sleep(30)
 
 
 asyncio.run(store_runtime_data())
