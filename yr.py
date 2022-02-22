10d5160b032c0abd8d3cc47ecd64bb9ac30c3b84
Matej Pristak <matej.pristak@gmail.com>
2022-02-22 15:23:09 +0100

    fix yr

--- yr.py
+++ yr.py
@@ -1,7 +1,6 @@
 import asyncio
 import time
-from datetime import datetime, timedelta
-from pprint import pprint
+from datetime import datetime, time
 from config import influxConfig
 
 from influxdb_client import InfluxDBClient, Point
@@ -17,53 +16,31 @@ async def store_runtime_data():
     client = InfluxDBClient(url=influxConfig["url"], token=influxConfig["token"], org=influxConfig["org"])
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
+    d = datetime.today()
+    d_min = datetime.combine(d, time.min)
+    d_max = datetime.combine(d, time.max)
+
     # print(forecast)
     write_api.write(bucket=influxConfig["bucket"], record=Point("WeatherForecast")
                     .tag("day", "today")
                     .field("maxtemp",
-                           max(forecast.data.intervals_between(datetime.today(), datetime.today() + timedelta(days=1)),
+                           max(forecast.data.intervals_between(d_min, d_max),
                                key=lambda x: x.variables["air_temperature"].value).variables["air_temperature"].value))
     write_api.write(bucket=influxConfig["bucket"], record=Point("WeatherForecast")
                     .tag("day", "today")
                     .field("mintemp",
-                           min(forecast.data.intervals_between(datetime.today(), datetime.today() + timedelta(days=1)),
+                           min(forecast.data.intervals_between(d_min, d_max),
                                key=lambda x: x.variables["air_temperature"].value).variables["air_temperature"].value))
     write_api.write(bucket=influxConfig["bucket"], record=Point("WeatherForecast")
                     .tag("day", "today")
                     .field("maxwind",
-                           max(forecast.data.intervals_between(datetime.today(), datetime.today() + timedelta(days=1)),
+                           max(forecast.data.intervals_between(d_min, d_max),
                                key=lambda x: x.variables["wind_speed"].value).variables["wind_speed"].value * 3.6))
     write_api.write(bucket=influxConfig["bucket"], record=Point("WeatherForecast")
                     .tag("day", "today")
                     .field("precip",
                            sum(map(lambda x: x.variables["precipitation_amount"].value,
-                                   forecast.data.intervals_between(datetime.today(),
-                                                                   datetime.today() + timedelta(days=1))))))
-
-    write_api.write(bucket=influxConfig["bucket"], record=Point("WeatherForecast")
-                    .tag("day", "tomorrow")
-                    .field("maxtemp",
-                           max(forecast.data.intervals_between(datetime.today() + timedelta(days=1),
-                                                               datetime.today() + timedelta(days=2)),
-                               key=lambda x: x.variables["air_temperature"].value).variables["air_temperature"].value))
-    write_api.write(bucket=influxConfig["bucket"], record=Point("WeatherForecast")
-                    .tag("day", "tomorrow")
-                    .field("mintemp",
-                           min(forecast.data.intervals_between(datetime.today() + timedelta(days=1),
-                                                               datetime.today() + timedelta(days=2)),
-                               key=lambda x: x.variables["air_temperature"].value).variables["air_temperature"].value))
-    write_api.write(bucket=influxConfig["bucket"], record=Point("WeatherForecast")
-                    .tag("day", "tomorrow")
-                    .field("maxwind",
-                           max(forecast.data.intervals_between(datetime.today() + timedelta(days=1),
-                                                               datetime.today() + timedelta(days=2)),
-                               key=lambda x: x.variables["wind_speed"].value).variables["wind_speed"].value * 3.6))
-    write_api.write(bucket=influxConfig["bucket"], record=Point("WeatherForecast")
-                    .tag("day", "tomorrow")
-                    .field("precip",
-                           sum(map(lambda x: x.variables["precipitation_amount"].value,
-                                   forecast.data.intervals_between(datetime.today() + timedelta(days=1),
-                                                                   datetime.today() + timedelta(days=2))))))
+                                   forecast.data.intervals_between(d_min, d_max)))))
 
 asyncio.run(store_runtime_data())
     # time.sleep(1800)
