20602021321fc7d6314400a5f52e4628a0f1a360
Matej Pristak <matej.pristak@gmail.com>
2022-09-02 15:20:24 +0200

    wallbox fix

--- wallbox.py
+++ wallbox.py
@@ -6,9 +6,7 @@ from os import truncate
 import requests as req
 
 import goodwe
-from config import influxConfig, inverterConfig
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
+from config import inverterConfig, wallboxConfig
 
 
 # adjust amps
@@ -47,25 +45,29 @@ def calculate_current(inverter, actual_charging_current):
     max_i2 = max_amp - (inverter["load_p2"] / 230 + inverter["backup_i2"])
     max_i3 = max_amp - (inverter["load_p3"] / 230 + inverter["backup_i3"])
     max_possible_current = min(max_i1, max_i2, max_i3)
-    available_current = min(max_possible_current, (inverter["active_power"] / 690) - actual_charging_current)  # 230 * 3
+    available_current = min(max_possible_current,
+                            (inverter["ppv"] / 690 - inverter["house_consumption"] / 690)) # 230 * 3
     allowable_current = actual_charging_current + available_current - 0.2  # 345W min. reserve
     allowable_current_capped = min(max_amp - 1, max(stop_at, math.floor(allowable_current)))  # step down by 1A
     would_add = (allowable_current - actual_charging_current)
 
     if was_charging:
         should_charge = allowable_current >= stop_at or inverter["battery_soc"] >= 80
-        # print(f"if charging, would charge: {should_charge}, {allowable_current_capped} A")
+        print(f"if charging, would charge: {should_charge}, {allowable_current_capped} A")
 
     if not was_charging:
-        should_charge = allowable_current >= start_at
+        should_charge = allowable_current >= start_at and inverter["battery_soc"] > 95
         # print(f"if NOT charging, would charge: {should_charge}, {allowable_current_capped} A")
 
     # print(f"P1 avail {max_i1} A")
     # print(f"P2 avail {max_i2} A")
     # print(f"P3 avail {max_i3} A")
+    # print(f"PPV {inverter['ppv']} W")
     # print(f"max {max_possible_current} A")
     # print(f"avail {available_current} A")
     # print(f"allowable {allowable_current} A")
+    # print(f"actual charging current {actual_charging_current} A")
+
     # print(f"would add {would_add} A")
 
     if not should_charge:
@@ -75,10 +77,6 @@ def calculate_current(inverter, actual_charging_current):
 
 
 async def wallbox():
-    client = InfluxDBClient(url=influxConfig["url"], token=influxConfig["token"], org=influxConfig["org"])
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-    read_api = client.query_api()
-
     inverter = await goodwe.connect(inverterConfig["ip_address"])
     runtime_data = await inverter.read_runtime_data()
 
@@ -86,39 +84,36 @@ async def wallbox():
     #     if sensor.id_ in runtime_data:
     #         print(f"{sensor.id_}: \t\t {sensor.name} = {runtime_data[sensor.id_]} {sensor.unit}")
 
-    bucket = influxConfig["bucket"]
-
-    previous_charging_curr_arr = read_api.query(f'''
-             from(bucket:"{bucket}") |> range(start: -5m)
-                 |> filter(fn: (r) => r["_measurement"] == "FVE")
-                 |> filter(fn: (r) => r["_field"] == "car_charge_amp")
-                 |> last()
-             ''')
-    previous_charging_curr = 0 if len(previous_charging_curr_arr) == 0 else \
-        previous_charging_curr_arr[0].records[0].values["_value"]
-
-    # req.get('http://192.168.1.118/api/status?filter=amp,alw,frc')
-    # res = json.loads(response.text)
-    # if !res["alw"]:
-    #    return; # no charge allowed - perhaps car not connected?
-    # previous_charging_curr = res["amp"] if res["frc"] == 0 else 0
+    response = req.get(wallboxConfig["address"] + 'api/status?filter=amp,alw,frc,car')
+    res = json.loads(response.text)
+    if res["car"] in [0, 1, 5]:
+        # print(f"no charge allowed - perhaps car not connected or doesn't want to charge, car state {res['car']}")
+        if res["frc"] != 1:
+            # print("setting force state to disabled")
+            req.get(wallboxConfig["address"] + 'api/set?frc=1')
+        return  # no charge allowed - perhaps car not connected, doesn't want to charge or disabled in app?
+    previous_charging_curr = res["amp"] if res["frc"] == 0 else 0
 
     charging_curr = calculate_current(runtime_data, previous_charging_curr)
 
-    # req.get('http://192.168.1.118/api/set?amp={charging_curr}')
-    # if res["frc"] == 1 && charging_curr > 0: # stopped, but should start
-    # req.get('http://192.168.1.118/api/set?amp={charging_curr}&frc=0')
-    # if res["frc"] == 0 && charging_curr > 0: # started, just change amp
-    #    if (res["amp"] == charging_curr)
-    #       do nothing
-    #    else
-    #       req.get('http://192.168.1.118/api/set?amp={charging_curr}')
-    # if res["frc"] == 1 && charging_curr == 0: # stopped, shouldn't start
-    # do nothing
-    # if res["frc"] == 0 && charging_curr == 0: # started, should stop
-    # req.get('http://192.168.1.118/api/set?frc=1')
-
-    write_api.write(bucket=influxConfig["bucket"], record=Point("FVE").field("car_charge_amp", int(charging_curr)))
+    if res["frc"] == 1 and charging_curr > 0:  # stopped, but should start
+        # print(f"stopped, but should start, {charging_curr}A")
+        req.get(wallboxConfig["address"] + 'api/set?amp={charging_curr}&frc=0')
+    if res["frc"] == 0 and charging_curr > 0:  # started, just change amp
+        if res["amp"] == charging_curr:
+            # print(f"started, do nothing, {charging_curr}A")
+            return
+        #       do nothing
+        else:
+            # print(f"started, just change amp, {charging_curr}A")
+            req.get(f'{wallboxConfig["address"]}api/set?amp={charging_curr}')
+    if res["frc"] == 1 and charging_curr == 0:  # stopped, shouldn't start
+        # print(f"stopped, shouldn't start, {charging_curr}A")
+        return
+        # do nothing
+    if res["frc"] == 0 and charging_curr == 0:  # started, should stop
+        # print(f"started, should stop, {charging_curr}A")
+        req.get(wallboxConfig["address"] + 'api/set?frc=1')
 
 
 test_data = {
