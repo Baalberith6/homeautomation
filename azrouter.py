e079f7a6db7abe2bd2ddb078cb74c05545b11256
Matej Pristak <matej.pristak@gmail.com>
2022-10-27 17:58:01 +0200

    azrouter change to consumer for inverter.py

--- azrouter.py
+++ azrouter.py
@@ -1,15 +1,11 @@
-import asyncio
-import time
-from pprint import pprint
+import json
 
 import requests
 
-from config import azrouterConfig, influxConfig
-
-from influxdb_client import InfluxDBClient, Point
-from influxdb_client.client.write_api import SYNCHRONOUS
-
-from secret import influxToken
+from common import connect_mqtt, publishProperties
+from config import azrouterConfig
+from config import generalConfig as c
+from paho.mqtt import client as mqtt_client
 
 
 def _request(path: tuple):
@@ -18,16 +14,31 @@ def _request(path: tuple):
     return r.json()
 
 
-async def store_runtime_data():
+def process_inverter_message(msg, client):
     r = _request(("api", "v1", "power"))
 
-    # pprint(r)
+    res = float(msg) - (r["input"]["power"][0]["value"] + r["input"]["power"][1]["value"] + r["input"]["power"][2]["value"])
+    res = 0 if res < 200 else res
 
-    client = InfluxDBClient(url=influxConfig["url"], token=influxToken, org=influxConfig["org"])
-    write_api = client.write_api(write_options=SYNCHRONOUS)
+    client.publish("home/FVE/pretoky/bojlery", res, qos=2, properties=publishProperties)
+    client.publish("home/FVE/pretoky/siet", r["input"]["power"][0]["value"] + r["input"]["power"][1]["value"] + r["input"]["power"][2]["value"], qos=2, properties=publishProperties)
 
-    write_api.write(bucket=influxConfig["bucket"], record=Point("FVE").tag("sum", "today").field("saved_energy", r["output"]["energy"][4]["value"]))
-    write_api.write(bucket=influxConfig["bucket"], record=Point("FVE").field("saved_energy", r["output"]["power"][0]["value"] + r["output"]["power"][1]["value"]))
 
-asyncio.run(store_runtime_data())
-    # time.sleep(15)
+def subscribe(client: mqtt_client, topics: [str]):
+    def on_message(client, userdata, msg):
+        if c["debug"]: print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        process_inverter_message(json.loads(msg.payload), client)
+
+    for topic in topics:
+        client.subscribe(topic)
+    client.on_message = on_message
+
+
+def run():
+    client = connect_mqtt("az")
+    subscribe(client, ["home/FVE/meter_power"])
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
