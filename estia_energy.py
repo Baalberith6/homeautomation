acf18165a3cea93c65fc0cba0e9486ff32f98df6
Matej Pristak <matej.pristak@vacuumlabs.com>
2024-01-28 12:31:41 +0100

    fix COP calc

--- estia_energy.py
+++ estia_energy.py
@@ -18,43 +18,41 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 influx_client = InfluxDBClient(url=influxConfig["url"], token=influxToken, org=influxConfig["org"])
 write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 
-hour_calculated = datetime.now().hour - 1
-cop = 0
-temp = 0
 api = ToshibaAcHttpApi(toshibaUsername, toshibaSecret)
 connected = False
 heat_loss = 143 # W/K
+temps = [18] * 24
 
-def calculate_cop(today_usage: int, last_hour_usage: int, old_cop: float, temp: float):
-    if c["debug"]: print(f"today: {today_usage}, last hour: {last_hour_usage}, old_cop: {old_cop}, temp: {temp}")
-    if temp > 17:
-        return old_cop
+def calculate_cop(today_usages: list, today_temp_avgs: list):
+    if c["debug"]: print(f"today consumption: {today_usages},\ntemps: {today_temp_avgs}")
 
-    new_cop = (heat_loss * (18 - temp)) / last_hour_usage
-    return (today_usage * old_cop + last_hour_usage * new_cop) / (today_usage + last_hour_usage)
+    cop = 0
+    total_consumption = 0
+
+    for hourly_temp, hourly_consumption in zip(today_temp_avgs, today_usages):
+        if hourly_temp > 17 or hourly_consumption is 0:
+            continue
+        new_cop = (heat_loss * (18 - hourly_temp)) / hourly_consumption
+        cop = (total_consumption * cop + hourly_consumption * new_cop) / (hourly_consumption + total_consumption)
+        if c["debug"]: print(f"HOURLY: {hourly_temp}C   {hourly_consumption}Wh  -> COP {cop}")
+        total_consumption += hourly_consumption
+
+    if c["debug"]: print(f"COP: {cop}")
+    return cop
 
 async def calc():
-    global connected, cop, api, temp, hour_calculated
+    global connected, api
 
     await api.connect()
     await api.get_devices()
 
     while True:
-        await asyncio.sleep(300)
-        if hour_calculated != datetime.now().hour:
-            hourly_usage = (await api.get_hourly_consumption(estiaConfig["device_unique_id"], datetime.now()))[0]["EnergyConsumption"]
-            last_hour_usage = hourly_usage[hour_calculated]["Energy"]
-            today_usage = 0
-            for hourly in hourly_usage:
-                if int(hourly["Time"]) == hour_calculated:
-                    break
-                today_usage += hourly["Energy"]
-            cop = calculate_cop(last_hour_usage, today_usage, cop, float(temp))
-            if c["debug"]: print(f"COP: {cop}, last hour used: {last_hour_usage}Wh, today_usage: {today_usage}Wh")
+        await asyncio.sleep(30)
+        hourly_usage = (await api.get_hourly_consumption(estiaConfig["device_unique_id"], datetime.now()))[0]["EnergyConsumption"]
+        if hourly_usage is not None: # polnoc
+            energy_values = [item["Energy"] for item in hourly_usage]
+            cop = calculate_cop(energy_values, temps)
             write_api.write(bucket=influxConfig["bucket"], record=Point("Estia").field("cop", cop))
-            hour_calculated = datetime.now().hour
-            if hour_calculated == 0:
-                cop = 0
 
 def start_async_loop():
     loop = asyncio.new_event_loop()
@@ -64,17 +62,17 @@ def start_async_loop():
 
 def subscribe(client: mqtt_client, topics: [str]):
     def on_message(client, userdata, msg):
-        global temp
+        global temps
         payload = msg.payload.decode()
         if c["debug"]: print(f"Received `{payload}` from `{msg.topic}` topic")
-        temp = float(payload)
+        temps[datetime.now().hour] = float(payload)
 
     for topic in topics:
         client.subscribe(topic)
     client.on_message = on_message
 
 def run():
-    client = connect_mqtt("estia_energy")
+    client = connect_mqtt("estia_energy2")
     subscribe(client, ["home/weather/local/temperature"])
     threading.Thread(target=start_async_loop, daemon=True).start()
     client.loop_forever()
--- tests/estua_energy_unit_test.py
+++ tests/estua_energy_unit_test.py
@@ -5,12 +5,12 @@ from estia_energy import calculate_cop
 
 class TestEstiaEnergy(unittest.TestCase):
 
-    def test_new(self):
-        self.assertEqual(2, calculate_cop(0, 1287, 0, 0)) # 18 * 143 = 2574, 2574 / 1287 = 2
-
-    def test_continuous(self):
-        self.assertEqual(2.6153846153846154, calculate_cop(1287, 572, 2, 2))  # 2574 + (16 * 143) / 1859 = 2.615
-        self.assertEqual(3, calculate_cop(1859, 143, 2.6153846153846154, 10))  # 2574 + 2288 + (8 * 143) / 2002 = 3
+    def test(self):
+        self.assertEqual(0, calculate_cop([0, 0, 0], [0,0,0]))
+        self.assertEqual(2, calculate_cop([1287, 0, 0], [0, 0, 0])) # 18 * 143 = 2574, 2574 / 1287 = 2
+        self.assertEqual(2.6153846153846154, calculate_cop([1287, 572, 0], [0, 2, 0]))  # 2574 + (16 * 143) / 1859 = 2.615
+        self.assertEqual(3, calculate_cop([1287, 572, 143], [0, 2, 10]))  # 2574 + 2288 + (8 * 143) / 2002 = 3
+        self.assertEqual(0.468, calculate_cop([1000, 10000], [0, 0]))
 
 if __name__ == '__main__':
     unittest.main()
